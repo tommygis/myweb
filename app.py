9e54e0dbedd1c21661a490337c09d68aac5b9f87
computer-hp <xiaofeigis@gmail>
2017-07-13 16:38:21 +0800

    001

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello/<name>')
-def hello_world(name):
-    return 'Hello %s' % name
+@app.route('/hello')
+def hello_world():
+    return 'Hello World'
 
+@app.route('/nihao')
+def hello_user():
+    return 'nihao'
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
