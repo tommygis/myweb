9824f5a13405bad4edf861f2fa26c97d805120ca
computer-hp <xiaofeigis@gmail>
2017-07-13 16:44:47 +0800

    001
    Signed-off-by: computer-hp <xiaofeigis@gmail>

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/nihao1')
+@app.route('/nihao')
 def hello_user():
-    return 'nihao1'
+    return 'nihao'
 
 if __name__ == '__main__':
     app.run()
