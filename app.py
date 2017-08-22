a33727ef3b17be27a02325eb69e8a9916f7b5c1d
computer-hp <xiaofeigis@gmail>
2017-08-22 17:09:57 +0800

    001
    Signed-off-by: computer-hp <xiaofeigis@gmail>

--- app.py
+++ app.py
@@ -4,18 +4,17 @@
 # @Site    : 
 # @File    : app.py
 # @Software: PyCharm
-from flask import Flask
+from flask import Flask, request
+from flask import render_template
 
 app = Flask(__name__)
 
-
 @app.route('/hello')
-def hello_world():
-    return 'Hello World'
-
-@app.route('/nihao11')
-def hello_user():
-    return 'nihao11'
+# def hello_world():
+#     return 'Hello World'
+@app.route('/hello/<name>')
+def hello_world(name = None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
